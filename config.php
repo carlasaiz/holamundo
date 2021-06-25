b6002889b7512cde72db569f56034e965484f2b1
carlasaiz <77211407+carlasaiz@users.noreply.github.com>
2021-06-25 13:09:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
  define( 'SITE_URL', 'http://ifcd0211.tk/ejercicios-php/mi-proyecto-V5'); //en producción sería otra url
 
- define( 'SITE_TIMEZONE', 'Europe/Brussels');
+ define( 'SITE_TIMEZONE', 'Europe/Madrid');
  define( 'SITE_LANG', ['es', 'spa', 'es_ES'] );
 
  define( 'DB_HOST', 'localhost' );
